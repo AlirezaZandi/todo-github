d2a2071c5f3dcf910b3f7b401b27df0b4e320a0c
Alireza Zandi <alirezazandi7800@gmail.com>
2022-02-06 19:27:55 +0330

    add more otions in addTodo menu

--- src/components/AddTodoPopop.jsx
+++ src/components/AddTodoPopop.jsx
@@ -4,9 +4,12 @@ import {
   DialogTitle,
   DialogContent,
   DialogContentText,
+  Autocomplete,
   TextField,
   DialogActions,
   Button,
+  Box,
+  Typography,
 } from "@mui/material";
 import { v4 } from "uuid";
 import { useDispatch } from "react-redux";
@@ -33,20 +36,45 @@ const AddTodoPopop = ({ open, setOpen, statusId }) => {
       <form onSubmit={(e) => handleAddTodo(e)}>
         <DialogTitle>Add Item</DialogTitle>
         <DialogContent>
-          <DialogContentText>
-            Enter the Item title and click the add button
-          </DialogContentText>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="todo-title"
-            label="Todo Title"
-            type="text"
-            fullWidth
-            variant="outlined"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-          />
+          <Box sx={{}}>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="todo-title"
+              label="Title"
+              type="text"
+              fullWidth
+              variant="outlined"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              sx={{
+                marginBottom: "1rem",
+                "& input": {
+                  fontSize: "1.2rem",
+                },
+              }}
+            />
+
+            <TextField
+              id="todo-more-info"
+              label="More Info"
+              fullWidth
+              multiline
+              rows={4}
+              sx={{
+                marginBottom: "1rem",
+              }}
+            />
+
+            <Autocomplete
+              id="combo-box-tag"
+              options={["Bug", "Feature", "Enhancement", "Epic"]}
+              fullWidth
+              renderInput={(params) => (
+                <TextField sx={{ width: "50%" }} {...params} label="Tags" />
+              )}
+            />
+          </Box>
         </DialogContent>
         <DialogActions>
           <Button onClick={() => setOpen(false)}>Cancel</Button>
---
+++ src/pages/todoAppPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const todoAppPage = () => {
+  return <div></div>;
+};
+
+export default todoAppPage;
