18c4933144dfb219881497a2dbad2db7c7f03bd7
Alireza Zandi <alirezazandi7800@gmail.com>
2022-02-07 00:38:43 +0330

    possible fix deploy

--- src/components/common/SelectBadges.jsx
+++ src/components/common/SelectBadges.jsx
@@ -6,9 +6,7 @@ import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
-import Chip from "@mui/material/Chip";
 import Badges from "./Badges";
-import { Circle } from "@mui/icons-material";
 
 const ITEM_HEIGHT = 48;
 const ITEM_PADDING_TOP = 8;
